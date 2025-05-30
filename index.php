2d0190a8e1ae9a566b9bd82fc3cf3beeea2ab5a9
tamerelmenoufi <tamer@mohatron.com.br>
2025-05-30 09:02:06 -0400

    first commit

--- index.php
+++ index.php
@@ -11,6 +11,13 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </head>
 <body>
+
+    <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Abu Amr</span>
+    </div>
+    </nav>
+
     <div class="m-3">
         <div class="row">
             <div class="col-3">Menu</div>
