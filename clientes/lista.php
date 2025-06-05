2c5d2ea9fdb6f74a57a9055b6165f30f5f4780e9
tamerelmenoufi <tamer@mohatron.com.br>
2025-06-05 12:32:00 -0400

    first commit

--- clientes/lista.php
+++ clientes/lista.php
@@ -8,7 +8,7 @@
     echo $query = "SELECT * FROM `logs_wapp` where dados->>'$.chat_type' = 'group' and dados->>'$.group_id' = '120363254840427713'";
     $result = mysqli_query($con, $query);
     
-    while($d = mysqi_fetch_object($result)){
+    while($d = mysqli_fetch_object($result)){
         echo $d->codigo."<br>";
         ///////////////////////
     }
--- lib/includes.php
+++ lib/includes.php
@@ -3,6 +3,7 @@
     //$con = AppConnect('amr');
 
     $con = mysqli_connect("yobom.com.br","root","SenhaDoBanco", "bk_manaus");
+    mysqli_set_charset($con,'utf8');
 
     $md5 = md5(date("YmdHis").$_SERVER['PHP_SELF']);
 
