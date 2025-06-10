f36be62637d0bd3d30e8fe5c2c414868f491727e
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-06-11 02:28:21 +0300

    first commit

--- lib/includes.php
+++ lib/includes.php
@@ -1,9 +1,6 @@
 <?php
-    //include("mohinc.php");
-    //$con = AppConnect('amr');
-
-    $con = mysqli_connect("yobom.com.br","root","SenhaDoBanco", "bk_manaus");
-    mysqli_set_charset($con,'utf8');
+    include("/appinc/connect.php");
+    $con = AppConnect('bk_manaus');
 
     $md5 = md5(date("YmdHis").$_SERVER['PHP_SELF']);
 
